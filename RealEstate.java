b6e001558afad66e707c8ef457c84dfb966db865
Meri Terenti <32930984+meriterenti@users.noreply.github.com>
2018-11-18 00:21:41 +0400

    Update RealEstate.java

--- RealEstate.java
+++ RealEstate.java
@@ -1,23 +1,24 @@
 public class RealEstate{
+	String title;
 	int numberOfRooms;
 	int price;
 	String estateType; 
 	
-	public RealEstate(int rooms, int price, String type) {
+	public RealEstate(String title, int rooms, int price, String type) {
+		this.title = title;
 		this.numberOfRooms = rooms;
 		this.price = price;
 		this.estateType = type;
 	}
 	
 	private String showInfo() {
-		return "The type must be 'Home' or 'Appartement'";
+		return "The type must be 'Home' or 'Apartment'";
 	}
 	
 	public String getType() {
-		if(estateType == "Home" || estateType == "Appartement") {
+		if(estateType == "Home" || estateType == "Apartment") {
 			return estateType;
 		}
 		return this.showInfo();
 	}
-	
 }
