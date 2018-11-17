7dd55f6e2a14f8c8124a3e4b5495b34f4820e062
Meri Terenti <32930984+meriterenti@users.noreply.github.com>
2018-11-17 20:58:06 +0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+	public static void main(String[] args){
+		System.out.println(Factorial.compute(5));
+		RealEstate estate1 = new RealEstate(4, 5000, "Home");
+		System.out.println(estate1.getType());
+	}
+}
